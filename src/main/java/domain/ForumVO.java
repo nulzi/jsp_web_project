7bae28127862a9b14b39e090028946f35964585c
DESKTOP-MQL55RN\kimty <kimkty131@naver.com>
2022-11-18 14:31:41 +0900

    [Create] new project for web programming

---
+++ src/main/java/domain/ForumVO.java
@@ -0,0 +1,35 @@
+package domain;
+
+public class ForumVO {
+	private String index;
+	private String title;
+	private String content;
+	private String date;
+	
+	public String getIndex() {
+		return index;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public String getContent() {
+		return content;
+	}
+	public String getDate() {
+		return date;
+	}
+	
+	public void setIndex(String index) {
+		this.index = index;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	
+}
---
+++ src/main/webapp/forums/totalForum.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<div class="catergories_list_container">
+		youtube img
+		<ul>
+			<li>category1</li>
+			<li>category2</li>
+			<li>category3</li>
+		</ul>
+	</div>
+	<div class="forum_container">게시글</div>
+</body>
+</html>
