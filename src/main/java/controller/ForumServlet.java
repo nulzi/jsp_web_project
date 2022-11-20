0e4b236f6d52363962c8ebc18284ca5fba74cbac
DESKTOP-MQL55RN\kimty <kimkty131@naver.com>
2022-11-21 01:25:26 +0900

    [Update] ForumServlet
    move page totalForum.jsp to register.html
    add post in register.html
    show result about register success or fail

--- src/main/java/controller/ForumServlet.java
+++ src/main/java/controller/ForumServlet.java
@@ -1,11 +1,20 @@
 package controller;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import domain.ForumVO;
+import persistence.ForumDAO;
 
 /**
  * Servlet implementation class ForumServlet
@@ -27,7 +36,13 @@ public class ForumServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("Forum doGet()");
+		String cmdReq = "";
+		cmdReq = request.getParameter("cmd");
+		if (cmdReq.equals("add")) {
+			response.sendRedirect("register.html");
+		}
 	}
 
 	/**
@@ -35,7 +50,44 @@ public class ForumServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		System.out.println("Forum doPost()");
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html; charset=UTF-8");
+
+		String cmdReq = "";
+		String message = "";
+
+		cmdReq = request.getParameter("cmd");
+
+		if (cmdReq.equals("join")) {
+			HttpSession session = request.getSession();
+			String user_id = (String) session.getAttribute("user_id");
+			System.out.println(user_id);
+			Date today = new Date();
+			DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			String upload_date = dateFormat.format(today);
+			
+			ForumVO forumVO = new ForumVO();
+
+			forumVO.setCreator(request.getParameter("creator"));
+			forumVO.setCategory(request.getParameter("category"));
+			forumVO.setUser_id(user_id);
+			forumVO.setContent(request.getParameter("content"));
+			forumVO.setUpload_date(upload_date);
+
+			ForumDAO forumDAO = new ForumDAO();
+
+			if (forumDAO.add(forumVO))
+				message = "가입을 축하합니다";
+			else
+				message = "가입을 실패입니다";
+
+			request.setAttribute("greetings", message);
+			request.setAttribute("post", forumVO);
+
+			RequestDispatcher view = request.getRequestDispatcher("register_result.jsp");
+			view.forward(request, response);
+		}
 	}
 
 }
